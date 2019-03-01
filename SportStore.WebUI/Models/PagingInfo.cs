c3fb3926b34cca166ec279705ef0e7d3e4b47547
339 <339@339->
2019-03-01 10:55:56 +1000

    Изменен вызов метода Html.PageLinks в представлении

--- SportStore.WebUI/Models/PagingInfo.cs
+++ SportStore.WebUI/Models/PagingInfo.cs
@@ -8,11 +8,11 @@ namespace SportStore.WebUI.Models
     //класс для передачи данных между представлением и контроллером. Для удобства.
     public class PagingInfo
     {
-        public int TotalItems { get; set; }
-        public int ItemsPerPage { get; set; }
-        public int CurrentPage { get; set; }
+        public int TotalItems { get; set; } //всего обьектов
+        public int ItemsPerPage { get; set; } //количество обьектов на странице
+        public int CurrentPage { get; set; } //номер текущей страницы
 
-        public int TotalPages
+        public int TotalPages //всего страниц
         {
             get { return (int) Math.Ceiling((decimal) TotalItems / ItemsPerPage); }
         }
