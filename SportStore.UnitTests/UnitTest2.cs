a8341af5c24013515b00092e48de6d9b91d9c925
339 <339@339->
2019-03-11 16:11:32 +1000

    Закончили с категориями товаров.

---
+++ SportStore.UnitTests/UnitTest2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+using SportStore.WebUI.Controllers;
+using SportStore.WebUI.HtmlHelpers;
+using SportStore.WebUI.Models;
+using Moq;
+using System.Linq;
+
+namespace SportStore.UnitTests
+{
+    /// <summary>
+    /// Summary description for UnitTest2
+    /// </summary>
+    [TestClass]
+    public class UnitTest2
+    {
+        public UnitTest2()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //Arrange - подготовка
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 1, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 1, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 1, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 1, Name = "P5", Category = "Cat3"}
+            }.AsQueryable());
+
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+            //Act - действие
+            int res1 = ((ProductListViewModel)target.List("Cat1").Model).PagingInfo.TotalItems;
+            int res2 = ((ProductListViewModel)target.List("Cat2").Model).PagingInfo.TotalItems;
+            int res3 = ((ProductListViewModel)target.List("Cat3").Model).PagingInfo.TotalItems;
+            int resAll = ((ProductListViewModel)target.List(null).Model).PagingInfo.TotalItems;
+
+            //Assert - проверка
+            Assert.AreEqual(res1, 2);
+            Assert.AreEqual(res2, 2);
+            Assert.AreEqual(res3, 1);
+            Assert.AreEqual(resAll, 5);
+        }
+    }
+}
--- SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore.WebUI/Controllers/ProductController.cs
@@ -24,18 +24,23 @@ namespace SportStore.WebUI.Controllers
         //List() - метод действия, визуализирующий представление, показывающее полный список товаров.
         public ViewResult List(string category, int page = 1)
         {
+            //свойства класса представления модели
+            //public IEnumerable<Product> Products { get; set; }
+            //public PagingInfo PagingInfo { get; set; }
+            //public string CurrentCategory { get; set; }
             ProductListViewModel model = new ProductListViewModel
             {
                 Products = repository.Products
-                    .Where(p => p.Category == null || p.Category == category)
-                    .OrderBy(p => p.ProductID)
-                    .Skip((page - 1) * PageSize)
-                    .Take(PageSize),
+                                         .Where(p => p.Category == null || p.Category == category)
+                                         .OrderBy(p => p.ProductID)
+                                         .Skip((page - 1) * PageSize)
+                                         .Take(PageSize),
                 PagingInfo = new PagingInfo()
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    //TotalItems = repository.Products.Count()
+                    TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(x => x.Category == category).Count()
                 },
                 CurrentCategory = category
             };
