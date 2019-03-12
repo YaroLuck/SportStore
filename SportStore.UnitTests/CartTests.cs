30c6a73198226234093c017e0a5ed08a9397fab4
339 <339@339->
2019-03-12 16:15:59 +1000

    Реализованы модульные тесты для класса Cart(корзина).

---
+++ SportStore.UnitTests/CartTests.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using SportStore.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace SportStore.UnitTests
+{
+    /// <summary>
+    /// Summary description for CartTests
+    /// </summary>
+    [TestClass]
+    public class CartTests
+    {
+        public CartTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Can_Add_New_Lines() //тестирование добавления элемента в корзину
+        {
+            //Если данный объект Product добавляется в корзину в первый раз то мы хотим чтобы был добавлен новый объект CartLine
+            //Arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            Cart target = new Cart();
+            // Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 2);
+            CartLine[] results = target.Lines.ToArray();
+            //Assert
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Product, p1);
+            Assert.AreEqual(results[1].Product, p2);
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines() //если Product уже есть в корзине, мы хотим увеличить количество в обьекте CartLine и не создавать новый.
+        {
+            //Arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            Cart target = new Cart();
+            //Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            CartLine[] results = target.Lines.OrderBy(c => c.Product.ProductID).ToArray();
+            //Assert
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Quantity, 11);
+            Assert.AreEqual(results[1].Quantity, 1);
+              
+        }
+
+        [TestMethod]
+        public void Can_Remove_Line() // удаление товаров из корзины
+        {
+            //Arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+
+            Cart target = new Cart();
+            //Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+            target.RemoveLine(p2);
+            //CartLine[] results = target.Lines.ToArray();
+            //Assert
+            Assert.AreEqual(target.Lines.Where(c => c.Product == p2).Count(), 0);
+            Assert.AreEqual(target.Lines.Count(), 2);
+
+           // Assert.AreEqual();
+        }
+
+        [TestMethod]
+        public void Calculate_Cart_Total() //расчет общей стоимости товаров в корзине
+        {
+            //Arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            Cart target = new Cart();
+            //Act
+            target.AddItem(p1, 4);
+            target.AddItem(p2, 1);
+            decimal totalVal = target.ComputeTotalValue();
+            //Assert
+            Assert.AreEqual(totalVal, 450M);
+        }
+
+        [TestMethod]
+        public void Can_Clear_Contents()
+        {
+            //Arrange
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            Cart target = new Cart();
+            //Act
+            target.AddItem(p1, 4);
+            target.AddItem(p2, 1);
+            target.Clear();
+            //Assert
+            Assert.AreEqual( target.Lines.Count(), 0);
+        }
+    }
+}
