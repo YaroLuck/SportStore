0202411c25110dfc2512a37ded1c7947c567fe47
339 <339@339->
2019-05-16 17:58:58 +1000

    Реализация информации о доставке. В процессе

---
+++ SportStore.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//для валидации
+using System.ComponentModel.DataAnnotations;
+
+namespace SportStore.Domain.Entities
+{
+    //класс для предоставления клиенту полей для ввода информации о доставке
+    class ShippingDetails
+    {
+        [Required(ErrorMessage = "Please enter a name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage  = "Please enter the first address line")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Please")]
+    }
+}
