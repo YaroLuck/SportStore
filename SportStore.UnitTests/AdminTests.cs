428f8ce82fad7ed1168a9b896b145d89adce9f9a
339 <339@339->
2019-05-27 09:55:47 +1000

    Создаем макет для представлений администрирования.

---
+++ SportStore.UnitTests/AdminTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+using SportStore.WebUI.Controllers;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace SportStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Products()
+        {
+            //Arrange - create mock repo
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+            }.AsQueryable());
+            //Arrange - create a controller
+            AdminController target = new AdminController(mock.Object);
+            //Action
+            Product[] result = ((IEnumerable<Product>)target.Index().ViewData.Model).ToArray();
+            //Assert
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual("P1", result[0].Name);
+            Assert.AreEqual("P2", result[1].Name);
+            Assert.AreEqual("P3", result[2].Name);
+        }
+    }
+}
---
+++ SportStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportStore.Domain.Abstract;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: Admin
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
